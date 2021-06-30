2d5bd6451be1becae08f7b701c0a7357bfe43664
pratyush-kr <pratyush151198@gmail.com>
2021-06-30 17:03:24 +0530

    making RoundRobin Scheduler

--- CPU-Scheduling.cpp
+++ CPU-Scheduling.cpp
@@ -16,6 +16,10 @@ class Scheduler
 {
     protected:
         std::vector<Process> arr;
+    public:
+        virtual void get_array() = 0;
+        virtual void calculate() = 0;
+        virtual void show() = 0;
 };
 
 class RoundRobin : public Scheduler
@@ -32,12 +36,15 @@ class RoundRobin : public Scheduler
 int main()
 {
     char command[20];
+    Scheduler *Sc;
     while(1)
     {
         std::cout<<"command: ";
         std::cin>>command;
         if(!strcmp(command, "exit"))
             break;
+        else if(!strcmp(command, "RoundRobin"))
+            Sc = new RoundRobin();
     }
     return 0;
 }
@@ -47,6 +54,7 @@ void RoundRobin::get_array()
     char PID;
     int BT, AT;
     std::string str;
+    std::cout<<"PID BT AT\n";
     while(1)
     {
         std::cin>>str;
@@ -59,6 +67,4 @@ void RoundRobin::get_array()
 }
 
 void RoundRobin::calculate()
-{
-    
-}
+{}
