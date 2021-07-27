9f11026cf332d0effb249ef1eccf7c1ff0a855a6
pratyush-kr <pratyush151198@gmail.com>
2021-07-27 22:03:42 +0530

    test

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -412,5 +412,6 @@ void Priority::calculate()
         arr[i].waiting_time = arr[i].completion_time - arr[i].brustTime - arr[i].arrivalTime;
         arr[i].turn_around_time = arr[i].waiting_time + arr[i].brustTime;
         arr[i].response_time = arr[i].got_cpu_at - arr[i].arrivalTime;
+        
     }
 }
